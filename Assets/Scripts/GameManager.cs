e3dbda104d4172e448241cbabe28acdb3c5d5981
transcental <amber@transcental.dev>
2025-09-28 13:06:48 +0100

    game managing

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BeatmapData
+{
+    public Sprite sprite;
+    public float rarity;
+    public bool healing;
+    public bool damaging;
+}
+
+[Serializable]
+public class LevelData
+{
+    public string levelName;
+    public int difficulty;
+    public AudioClip backgroundMusic;
+    public TextAsset beatmap;
+    public List<BeatmapData> beats;
+    public Sprite backgroundImage;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+    private bool isPaused =  false;
+    public bool IsPaused { get { return isPaused; } }
+    private bool isPlaying = false;
+    public bool IsPlaying { get { return isPlaying; } }
+    [SerializeField] private List<LevelData> levels;
+    
+}
