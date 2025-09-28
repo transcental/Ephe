f2c2f1259af8d4fe02f5f198d708b9d73e8f0b70
transcental <amber@transcental.dev>
2025-09-28 15:26:18 +0100

    boopy

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,28 +1,55 @@
+using System;
 using UnityEngine;
+
+[Serializable]
+public class BeatmapJsonData
+{
+    public BeatmapDataItem[] beatmapData;
+}
+
+[Serializable]
+public class BeatmapDataItem
+{
+    public string timestamp;
+    public string type;
+}
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void SetupLevel()
+    [SerializeField] private GameObject topBeatSpawner;
+    [SerializeField] private GameObject bottomBeatSpawner;
+    [SerializeField] private GameObject topCannon;
+    [SerializeField] private GameObject bottomCannon;
+    private GameManager _gameManager;
+    private int _score = 0;
+    private int health = 5;
+    private bool _playing = false;
+    
+    
+    private void Awake()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+        if (_gameManager == null)
+        {
+            Debug.LogError("GameManager not found in the scene.");
+            return;
+        }
+        Debug.Log("LevelManager: Found GameManager.");
+    }
+    
+    
+    public void SetupLevel(LevelData levelData)
     {
         Debug.Log("LevelManager: Setting up level...");
-        var height = Camera.main.orthographicSize * 2;
-        var width = height * Camera.main.aspect;
-        Debug.Log($"Camera dimensions - Width: {width}, Height: {height}");
-        // Additional setup code here
         
-        // top bar will be a canvas 5% of the height of the screen
-        // bottom bar will be a canvas 10% of the height of the screen
-        var topBarTop = height / 2;
-        var topBarBottom = topBarTop - (height * 0.05f);
-        var bottomBarBottom = -height / 2;
-        var bottomBarTop = bottomBarBottom + (height * 0.1f);
-        Debug.Log($"Top Bar - Top: {topBarTop}, Bottom: {topBarBottom}");
-        Debug.Log($"Bottom Bar - Top: {bottomBarTop}, Bottom: {bottomBarBottom}");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_playing && !_gameManager.IsPaused) { return; }
         
-        var playAreaTop = topBarBottom;
-        var playAreaBottom = bottomBarTop;
         
+
     }
 
 }
